b758549b92f29b1288aaf0101243423675f7be07
Sergey Akimushkin <akimushkin@outlook.com>
2024-01-11 00:16:05 +0300

    Init

---
+++ src/main/java/com/devtiro/books/SpringDevtiroBooksApplication.java
@@ -0,0 +1,13 @@
+package com.devtiro.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevtiroBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevtiroBooksApplication.class, args);
+    }
+
+}
